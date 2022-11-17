4345d074f88b3937099552ae29caa3cb0103c58e
Sergii Bielskyi <sergey.belskiy@hotmail.com>
2022-11-17 21:31:21 +0000

    added conf reading

--- api/operations/Controllers/OperationsDaprController.cs
+++ api/operations/Controllers/OperationsDaprController.cs
@@ -8,20 +8,24 @@ public class OperationsDaprController : ControllerBase
 {
     private readonly ILogger<OperationsDaprController> _logger;
     DaprClient daprClient;
-    string storeName = "cosmosdbapp";
+     private readonly IConfiguration Configuration;
 
-    public OperationsDaprController(ILogger<OperationsDaprController> logger)
+
+    public OperationsDaprController(ILogger<OperationsDaprController> logger, IConfiguration configuration)
     {
         _logger = logger;
+        Configuration = configuration;
         daprClient = new DaprClientBuilder().Build();
+        _logger.LogInformation("Store name is - " + Configuration["storeName"]);
     }
 
     [HttpPost("/checkout")]
     public void getCheckout([FromBody] object order)
     {
+     
         _logger.LogInformation("Order is comming - " + order.ToString());
 
-        daprClient.SaveStateAsync(storeName, Guid.NewGuid().ToString(), order);
+        daprClient.SaveStateAsync(Configuration["storeName"], Guid.NewGuid().ToString(), order);
     }
 
 }
